ed5ad6a8ed8e46494281144002d2208bb19261df
Robert Landrito <rlandrito@gmail.com>
2022-10-16 08:53:52 +0200

    Cleanups

--- client/src/x11.rs
+++ client/src/x11.rs
@@ -1,4 +1,4 @@
-use std::convert::TryInto;
+use std::convert::TryFrom;
 
 use super::config::X11Config;
 use super::util::{connect_stream, either};
@@ -9,33 +9,10 @@ use super::CONFIG;
 use tokio::io::AsyncWriteExt;
 use tokio::net::UnixStream;
 
-fn get_display_num(stream: &UnixStream) -> Option<u8> {
-    let local_addr = stream.local_addr().ok()?;
-    let path = match local_addr.as_pathname() {
-        Some(path) => path,
-        None => todo!(),
-    };
-    let path_str = match path.to_str() {
-        Some(string) => string,
-        None => todo!()
-    };
-    println!("{}", path_str);
+async fn handle_stream(mut stream: UnixStream, display: u8) -> std::io::Result<()> {
+    println!("{}", display);
 
-    assert!(path_str.chars().count() > 0);
-    let display_str = path_str.chars().last().unwrap();
-
-    let display: u8 = display_str.to_digit(10).unwrap().try_into().unwrap();
-    assert!(display <=9);
-
-    return Some(display);
-}
-
-async fn handle_stream(mut stream: UnixStream) -> std::io::Result<()> {
     let mut server = VmSocket::connect(CONFIG.service_port).await?;
-
-    let display = get_display_num(&stream).unwrap();
-    //if display != 0 { return Ok(()); }
-
     server.write_all(b"x11\0").await?;
     server.write_u8(display).await?;
 
@@ -50,11 +27,13 @@ pub async fn x11_forward(config: &'static X11Config, display: u32) -> std::io::R
     let lock = X11Lock::acquire(display, config.force)?;
     let listener = lock.bind()?;
 
+    let display_u8 = u8::try_from(display).ok().unwrap();
+
     loop {
         let stream = listener.accept().await?.0;
 
         tokio::task::spawn(async move {
-            if let Err(err) = handle_stream(stream).await {
+            if let Err(err) = handle_stream(stream, display_u8).await {
                 eprintln!("Failed to transfer: {}", err);
             }
         });
